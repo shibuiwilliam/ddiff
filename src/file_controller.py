e3a16da1dc69dffa2aa8f78ae1d4b11e31f27248
Shibui Yusuke <yusuke.shibui@ShibuinoMacBook-Pro.local>
2020-12-22 12:52:21 +0900

    file controller

---
+++ src/file_controller.py
@@ -0,0 +1,63 @@
+import json
+import yaml
+from ruamel.yaml import YAML, add_constructor, resolver
+from collections import OrderedDict
+
+
+class FileController(object):
+    def __init__(self, file_0: str, file_1: str):
+        self.__file_0: str = file_0
+        self.__file_1: str = file_1
+
+        self.__data_0: OrderedDict = self.load_file(file=self.__file_0)
+        self.__data_1: OrderedDict = self.load_file(file=self.__file_1)
+
+    @property
+    def data_0(self) -> OrderedDict:
+        return self.__data_0
+
+    @property
+    def data_1(self) -> OrderedDict:
+        return self.__data_1
+
+    def load_file(self, file: str) -> OrderedDict:
+        if self.__is_yaml(file=file):
+            return self.__load_yaml(file=file)
+        if self.__is_json(file=file):
+            return self.__load_json(file=file)
+        raise Exception("File must be a yaml or json")
+
+    def __is_yaml(self, file: str) -> bool:
+        try:
+            with open(file, "r", encoding="utf-8") as f:
+                yaml.safe_load(f)
+            return True
+        except Exception as _:
+            return False
+
+    def __is_json(self, file: str) -> bool:
+        try:
+            with open(file, "r", encoding="utf-8") as f:
+                json.loads(f)
+            return True
+        except Exception as _:
+            return False
+
+    def __load_yaml(self, file: str) -> OrderedDict:
+        add_constructor(
+            resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+            lambda loader, node: OrderedDict(loader.construct_pairs(node)),
+        )
+
+        _yaml = YAML()
+        _yaml.default_flow_style = False
+
+        with open(file, "r", encoding="utf-8") as f:
+            data = _yaml.load(f)
+        return data
+
+    def __load_json(self, file: str) -> OrderedDict:
+        with open(file, "r", encoding="utf-8") as f:
+            data = json.loads(f, object_pairs_hook=OrderedDict)
+
+        return data
--- src/main.py
+++ src/main.py
@@ -1,25 +1,15 @@
-from ruamel.yaml import YAML, add_constructor, resolver
 from collections import OrderedDict
 import click
 
+from src.file_controller import FileController
+
 
 @click.command()
-@click.argument("file", nargs=1)
-@click.option("-f", "--file", "file", required=True, type=str, help="file path")
-def main(file: str):
-    add_constructor(
-        resolver.BaseResolver.DEFAULT_MAPPING_TAG,
-        lambda loader, node: OrderedDict(loader.construct_pairs(node)),
-    )
-
-    yaml = YAML()
-    yaml.default_flow_style = False
-
-    with open(file, "r", encoding="utf-8") as f:
-        data = yaml.load(f)
-
-    with open("/tmp/output.yaml", "w", encoding="utf-8") as f:
-        yaml.dump(data, f)
+@click.argument("files", nargs=2)
+def main(files: str):
+    file_controller = FileController(file_0=files[0], file_1=files[1])
+    print(file_controller.data_0)
+    print(file_controller.data_1)
 
 
 if __name__ == "__main__":
