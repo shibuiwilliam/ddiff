c2e9b4baa675dd9cef4d1d93e21cb13e6c276d59
Shibui Yusuke <yusuke.shibui@ShibuinoMacBook-Pro.local>
2020-12-22 16:26:38 +0900

    file controller

---
+++ src/data_diff.py
@@ -0,0 +1,42 @@
+from typing import Tuple
+from collections import OrderedDict, Counter
+
+
+def data_diff(
+    file_data_0: Tuple[str, OrderedDict],
+    file_data_1: Tuple[str, OrderedDict],
+    path: str = "",
+):
+    file_0 = file_data_0[0]
+    data_0 = file_data_0[1]
+    file_1 = file_data_1[0]
+    data_1 = file_data_1[1]
+    original_path = path
+    for key, value in data_0.items():
+        if original_path == "":
+            path = f"{key}"
+        else:
+            path = f"{original_path}.{key}"
+        if isinstance(value, OrderedDict):
+            data_diff((file_0, data_0[key]), (file_1, data_1[key]), path)
+        elif isinstance(value, list):
+            if key not in data_1.keys():
+                print(f"{file_0} + {path}")
+                print(f"{file_1} - {path}")
+            else:
+                if isinstance(data_1[key], list):
+                    counted_0 = Counter(value)
+                    counted_1 = Counter(data_1[key])
+                    if counted_0 != counted_1:
+                        print(f"{path}:")
+                        print(f"\t{file_0}: {data_0[key]}")
+                        print(f"\t{file_1}: {data_1[key]}")
+        else:
+            if key not in data_1.keys():
+                print(f"{file_0} + {path}")
+                print(f"{file_1} - {path}")
+            else:
+                if data_0[key] != data_1[key]:
+                    print(f"{path}:")
+                    print(f"\t{file_0}: {data_0[key]}")
+                    print(f"\t{file_1}: {data_1[key]}")
--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from collections import OrderedDict
 import click
 
 from src.file_controller import FileController
+from src.data_diff import data_diff
 
 
 @click.command()
@@ -10,6 +11,12 @@ def main(files: str):
     file_controller = FileController(file_0=files[0], file_1=files[1])
     print(file_controller.data_0)
     print(file_controller.data_1)
+
+    data_diff(
+        file_data_0=(files[0], file_controller.data_0),
+        file_data_1=(files[1], file_controller.data_1),
+        path="",
+    )
 
 
 if __name__ == "__main__":
